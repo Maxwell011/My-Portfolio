66295a10488176e0ddd23610562d2b6f3c0d0180
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-09-05 19:52:45 +0100

    Modified hamburger

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 // Getting Elements
-const toggleBtn = document.querySelector(".sidebar-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const sidebar = document.querySelector(".sidebar");
-const aboutMe = document.querySelector(".about");
-const contactMe = document.querySelector(".contacts");
-const portfolio = document.querySelector(".portfolio");
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+const aboutMe = document.querySelector('.about');
+const contactMe = document.querySelector('.contacts');
+const portfolio = document.querySelector('.portfolio');
 
-toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("show-sidebar");
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
 });
 
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
 });
 
-aboutMe.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+aboutMe.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
 });
 
-contactMe.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+contactMe.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
 });
 
-portfolio.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+portfolio.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
 });
+
+function resetMenu() {
+  if (window.innerWidth > 768) {
+    sidebar.style.display = 'none';
+  }
+}
+window.addEventListener('resize', resetMenu);
